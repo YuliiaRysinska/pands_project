a45fc0d2aadc847d5077ba3fc51fced22110ddca
Админ <iuy.nickolaencko@gmail.com>
2024-05-05 20:04:46 +0100

    reload data set

---
+++ analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import seaborn as sns 
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Loading iris dataset into panda data frame
+iris = pd.read_csv('iris.csv')
+print(iris)
