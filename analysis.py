a864c9f91651510a1e2011e598c503a0e925025a
Админ <iuy.nickolaencko@gmail.com>
2024-05-05 21:04:17 +0100

    general info of data

--- analysis.py
+++ analysis.py
@@ -2,7 +2,19 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
 import numpy as np
+import sklearn
+
 
 #Loading iris dataset into panda data frame
+file_name = 'iris.csv'
 iris = pd.read_csv('iris.csv')
-print(iris)
+#view of general data
+print(iris.head(1))
+print(iris.info())
+print(iris.describe()) 
+print(iris['species'].value_counts())
+
+#visual inspection of whether there is a relationship between independent and dependent metrics (categories)
+
+
+
