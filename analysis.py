73f912f68c9ab26fe412ce99d54566c53602229a
Админ <iuy.nickolaencko@gmail.com>
2024-05-06 19:39:58 +0100

    creating jupyter notebook

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+#using necessary libraries
 import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
@@ -8,13 +9,14 @@ from sklearn.metrics import accuracy_score
 
 
 #Loading iris dataset into panda data frame
-file_name = 'iris.csv'
+
 iris = pd.read_csv('iris.csv')
 #view of general data
-print(iris.head(1))
+print(iris.head(10))
 print(iris.info())
 print(iris.describe()) 
 print(iris['species'].value_counts())
+
 
 #visual inspection of whether there is a relationship between independent and dependent metrics (categories)
 sns.pairplot(data = iris, hue = 'species')
@@ -25,7 +27,7 @@ y = iris['species']
 x = iris.drop(columns = 'species')
 print("Total lens", len(x), len(y))
 
-#formation of separate sets for forecast yodel training and testing
+#formation of separate sets for forecast model training and testing
 x_train, x_test, y_train, y_test = train_test_split(x,y)
 
 print("Train len", len(x_train), len(y_train))
@@ -40,7 +42,3 @@ random_y_test = np.random.choice(np.unique(y), size=len(y_test))
 random_accuracy_score = accuracy_score(y_test, random_y_test)
 print("Random accuracy score = ", random_accuracy_score)
 
-#
-# https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
-knn = KNeighborsClassifier(n_neighbors=5)
-knn.fit(x_train, y_train)
